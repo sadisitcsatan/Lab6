a9e46daf9a985228b232c4ae1d3757df25f5b09a
Chris Scott <chrscott@csumb.edu>
2019-11-04 09:33:59 -0800

    fixed lab 7 route

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
 app.get('/lab7',function (req,res,next) {
-    res.render("lab7/earth.html");
+    res.render("Lab7/index.html");
 });
 // app.post("/lab7", async function (req,res,next) {
 //     //console.log(req.query.q, req.query.orientatio);
